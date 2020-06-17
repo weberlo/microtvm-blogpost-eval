b5a785f5e0d040731a6706e5ec27485c58eb4a7e
Logan Weber <weberlo@cs.washington.edu>
2020-06-17 14:31:44 -0700

    clean up

--- python/micro_eval/model/quantize_cifar10.py
+++ python/micro_eval/model/quantize_cifar10.py
@@ -24,8 +24,7 @@ class QuantPrefixCutter(ExprMutator):
         return var
 
     def visit_call(self, call):
-        print(call.checked_type.dtype)
-        if call.op.name == 'cast' and call.checked_type.dtype == 'int8':
+        if call.op.name == 'cast' and call.attrs['dtype'] == 'int8':
             res = super().visit_call(call)
             if len(self.subtree_params) == 0:
                 return res
@@ -41,10 +40,8 @@ class QuantPrefixCutter(ExprMutator):
 class QuantSuffixCutter(ExprMutator):
     def __init__(self):
         ExprMutator.__init__(self)
-        self.pre_cast_expr = None
 
     def visit_call(self, call):
-        # print(call.checked_type.dtype)
         if call.op.name == 'annotation.stop_fusion':
             return call
         else:
@@ -55,31 +52,22 @@ class QuantSuffixCutter(ExprMutator):
                     return arg_res
                 else:
                     new_args.append(arg_res)
-            return relay.Call(call.func, new_args, call.attrs)
-
-#    def visit_function(self, func):
-#        # import pdb; pdb.set_trace()
-#        # new_params = [self.visit(x) for x in fn.params]
-#        sig_rewriter = SignatureRewriter()
-#        new_params = [
-#            relay.Var(
-#                param.name_hint,
-#                sig_rewriter.visit(param.type_annotation)
-#            )
-#            for param in func.params]
-#        new_body =
-#        new_ret_type = sig_rewriter.visit(func.ret_type)
-#        return relay.Function(
-#            func.params,
-#            self.visit(func.body),
-#            func.ret_type,
-#            func.type_params,
-#            func.attrs)
+            return relay.Call(call.op, new_args, call.attrs)
 
 
 class SignatureRewriter(TypeMutator):
     def visit_tensor_type(self, tt):
         return relay.TensorType(tt.shape, 'int8')
+
+
+class VarReplacer(ExprMutator):
+    def __init__(self, replace_map):
+        ExprMutator.__init__(self)
+        self.replace_map = replace_map
+
+    def visit_var(self, var):
+        if var in self.replace_map:
+            return self.replace_map[var]
 
 
 def with_int_signature(func):
@@ -91,37 +79,37 @@ def with_int_signature(func):
         )
         for param in func.params]
     new_ret_type = sig_rewriter.visit(func.ret_type)
+    new_body = VarReplacer(dict(zip(func.params, new_params))).visit(func.body)
     return relay.Function(
         new_params,
-        func.body,
+        new_body,
         new_ret_type,
         func.type_params,
         func.attrs)
 
 
 def partition_quantized(mod):
-    # TODO
-    # - change to expr mutator
-    # - match cast ops then call a separate visitor that finds the param at the bottom of the chain
-    # - return the result from the visitor
-    # - change types in function signature (use type functor?)
+    # TODO we have the prefix/suffix conversion code chopped off, but we want
+    # it to be a *partition*, so we need to save the pieces we're cutting off.
+    # Also, do we want any restrictions on how much gets cut off?
+    # Right now, with the CIFAR-10 CNN, it cuts off some bias add and dense
+    # operations (i.e., not just conversion ops like clip, cast, and round),
+    # causing type inference to fail.
     assert len(mod.functions) == 1
     func = mod['main']
     func = with_int_signature(func)
-
+    print('[With Int Signature]')
+    print(func)
     prefix_cutter = QuantPrefixCutter(func.params)
     func = prefix_cutter.visit(func)
     print('[Without Conversion Prefix]')
     print(func)
-
     suffix_cutter = QuantSuffixCutter()
     func = suffix_cutter.visit(func)
     print('[Without Conversion Suffix]')
     print(func)
-
     mod['main'] = func
     mod = transform.InferType()(mod)
-    import pdb; pdb.set_trace()
     return mod
 
 
@@ -168,22 +156,7 @@ def quantize_cifar10():
   quantize(mod, params)
 
 
-def quantize_test():
-    # """Program:
-    #    fn (%x : Tensor[(10, 10), float32]) {
-    #      let %t1 = log(x);
-    #      let %t2 = add(%t1, %x);
-    #      %t1
-    #    }
-    # """
-    # tp = relay.TensorType((10, 10), "float32")
-    # x = relay.var("x", tp)
-    # sb = relay.ScopeBuilder()
-    # t1 = sb.let("t1", relay.log(x))
-    # t2 = sb.let("t2", relay.add(t1, x))
-    # sb.ret(t2)
-    # f = relay.Function([x], sb.get())
-
+def test_conv_quant():
     dshape = (1, 4, 16, 16)
     # TODO quantization shouldn't crash if a pre-quantized graph (already int8) is handed to it.
     dtype = 'float32'
@@ -208,6 +181,29 @@ def quantize_test():
     quantize(mod, params)
 
 
+def test_add_quant():
+    func = relay.fromtext("""
+    v0.0.4
+    fn (%x : Tensor[(10, 10), float32],
+        %y : Tensor[(10, 10), float32]) {
+      add(%x, %y)
+    }
+    """)
+    mod = tvm.IRModule.from_expr(func)
+    mod = transform.InferType()(mod)
+    params = {}
+    quantize(mod, params)
+    # """
+    # tp = relay.TensorType((10, 10), "float32")
+    # x = relay.var("x", tp)
+    # sb = relay.ScopeBuilder()
+    # t1 = sb.let("t1", relay.log(x))
+    # t2 = sb.let("t2", relay.add(t1, x))
+    # sb.ret(t2)
+    # f = relay.Function([x], sb.get())
+
+
 if __name__ == '__main__':
-#   quantize_cifar10()
-  quantize_test()
+  quantize_cifar10()
+#   test_conv_quant()
+#   test_add_quant()
