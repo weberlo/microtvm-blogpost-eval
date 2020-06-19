346630eec12d0de5a33ff76f1121adb9392248ae
Logan Weber <weberlo@cs.washington.edu>
2020-06-19 16:32:29 -0700

    finish prefix partitioning

--- python/micro_eval/model/quantize_cifar10.py
+++ python/micro_eval/model/quantize_cifar10.py
@@ -1,5 +1,6 @@
 import argparse
 
+import numpy as np
 from mxnet import nd, autograd, gluon
 from mxnet.gluon.data.vision import transforms
 import onnx
@@ -12,11 +13,26 @@ from tvm.relay import transform
 from micro_eval import util
 from micro_eval.util import model_util
 
+# TODO don't hardcode so many things as int8
+
+def with_dtype(ty, target_dtype):
+    class DtypeReplacer(TypeMutator):
+        def __init__(self, target_dtype):
+            self.target_dtype = target_dtype
+
+        def visit_tensor_type(self, tt):
+            return relay.TensorType(tt.shape, self.target_dtype)
+
+    return DtypeReplacer(target_dtype).visit(ty)
+
+
 class QuantPrefixCutter(ExprMutator):
     def __init__(self, params):
         ExprMutator.__init__(self)
         self.params = set(params)
         self.subtree_params = set()
+        self.new_func_params = []
+        self.prefix_sb = relay.ScopeBuilder()
 
     def visit_var(self, var):
         if var in self.params:
@@ -30,11 +46,47 @@ class QuantPrefixCutter(ExprMutator):
                 return res
             else:
                 assert len(self.subtree_params) == 1
-                res = next(iter(self.subtree_params))
+                param = next(iter(self.subtree_params))
+                self.prefix_sb.let(param.name_hint, res)
                 self.subtree_params.clear()
-                return res
+                # return new parameter, then we can use
+                # relay.analysis.free_vars at the end of the pass to generate
+                # new `mid_func` type signature
+                return relay.Var(param.name_hint, with_dtype(param.type_annotation, 'int8'))
         else:
             return super().visit_call(call)
+
+    def visit_function(self, func):
+        # override to make sure we *don't* visit the function params
+        return relay.Function(
+            func.params,
+            self.visit(func.body),
+            func.ret_type,
+            func.type_params,
+            func.attrs)
+
+
+def partition_prefix(mod):
+    assert len(mod.functions) == 1
+    func = mod['main']
+    prefix_cutter = QuantPrefixCutter(func.params)
+    mid_body = prefix_cutter.visit(func.body)
+    mid_func = relay.Function(
+        relay.analysis.free_vars(mid_body),
+        mid_body,
+        func.ret_type,
+        func.type_params,
+        func.attrs)
+    mid_mod = tvm.IRModule.from_expr(mid_func)
+
+    scope_builder = prefix_cutter.prefix_sb
+    ret_expr = relay.Tuple(list(map(lambda b: b[0], scope_builder._bindings[-1])))
+    scope_builder.ret(ret_expr)
+    pre_func_body = scope_builder.get()
+    pre_func = relay.Function(relay.analysis.free_vars(pre_func_body), pre_func_body)
+    pre_mod = tvm.IRModule.from_expr(pre_func)
+
+    return pre_mod, mid_mod
 
 
 class QuantSuffixCutter(ExprMutator):
@@ -49,43 +101,58 @@ class QuantSuffixCutter(ExprMutator):
             for arg in call.args:
                 arg_res = self.visit(arg)
                 if type(arg_res) == relay.Call and arg_res.op.name == 'annotation.stop_fusion':
+                    # import pdb; pdb.set_trace()
                     return arg_res
                 else:
                     new_args.append(arg_res)
             return relay.Call(call.op, new_args, call.attrs)
 
-
-class SignatureRewriter(TypeMutator):
-    def visit_tensor_type(self, tt):
-        return relay.TensorType(tt.shape, 'int8')
-
-
-class VarReplacer(ExprMutator):
-    def __init__(self, replace_map):
-        ExprMutator.__init__(self)
-        self.replace_map = replace_map
-
-    def visit_var(self, var):
-        if var in self.replace_map:
-            return self.replace_map[var]
+    #def visit_function(self, func):
+    #    new_body = self.visit(func.body)
+    #    return relay.Function(
+    #        relay.analysis.free_vars(new_body),
+    #        new_body,
+    #        func.ret_type,
+    #        func.type_params,
+    #        func.attrs)
 
 
-def with_int_signature(func):
-    sig_rewriter = SignatureRewriter()
-    new_params = [
-        relay.Var(
-            param.name_hint,
-            sig_rewriter.visit(param.type_annotation)
-        )
-        for param in func.params]
-    new_ret_type = sig_rewriter.visit(func.ret_type)
-    new_body = VarReplacer(dict(zip(func.params, new_params))).visit(func.body)
-    return relay.Function(
-        new_params,
-        new_body,
-        new_ret_type,
-        func.type_params,
-        func.attrs)
+
+def partition_suffix(mod):
+    assert len(mod.functions) == 1
+    func = mod['main']
+    suffix_cutter = QuantSuffixCutter()
+    func = suffix_cutter.visit(func)
+
+    assert False, 'actually partition it'
+
+
+# class VarReplacer(ExprMutator):
+#     def __init__(self, replace_map):
+#         ExprMutator.__init__(self)
+#         self.replace_map = replace_map
+#
+#     def visit_var(self, var):
+#         if var in self.replace_map:
+#             return self.replace_map[var]
+#
+#
+# def with_int_signature(func):
+#     sig_rewriter = SignatureRewriter()
+#     new_params = [
+#         relay.Var(
+#             param.name_hint,
+#             sig_rewriter.visit(param.type_annotation)
+#         )
+#         for param in func.params]
+#     new_ret_type = sig_rewriter.visit(func.ret_type)
+#     new_body = VarReplacer(dict(zip(func.params, new_params))).visit(func.body)
+#     return relay.Function(
+#         new_params,
+#         new_body,
+#         new_ret_type,
+#         func.type_params,
+#         func.attrs)
 
 
 def partition_quantized(mod):
@@ -95,17 +162,29 @@ def partition_quantized(mod):
     # Right now, with the CIFAR-10 CNN, it cuts off some bias add and dense
     # operations (i.e., not just conversion ops like clip, cast, and round),
     # causing type inference to fail.
+    #
+    # should the user receive diagnostics about the results (e.g., letting them
+    # know some operators were chopped off in the prefix/suffix?)
+    #
+    # keep in mind that we have an implicit assumption in the prefix cutter
+    # that the first operator is quantizable, which is fairly safe, since a CNN
+    # usually starts with a conv, but if we have plans on generalizing the
+    # quantization pass to arbitrary models, this will need to change.
+    #
+    # TODO TODO you just finished generalizing the quant prefix cutter to work
+    # with multiple input args (`test_multiple_arg_conversions`), but you still
+    # need to construct the prefix function (probably using a let list that
+    # builds bindings that convert each of the args and returns a tuple of all
+    # of them at the end)
+    #
+    # once it's all mostly working, test that `x |> prefix |> mid_func |> suffix`
+    # is the same as `x |> orig_func`
     assert len(mod.functions) == 1
-    func = mod['main']
-    func = with_int_signature(func)
-    print('[With Int Signature]')
-    print(func)
-    prefix_cutter = QuantPrefixCutter(func.params)
-    func = prefix_cutter.visit(func)
+    pre_mod, mid_mod = partition_prefix(mod)
     print('[Without Conversion Prefix]')
-    print(func)
-    suffix_cutter = QuantSuffixCutter()
-    func = suffix_cutter.visit(func)
+    print(mid_mod)
+    import pdb; pdb.set_trace()
+    mid_mod, post_mod = partition_suffix(mid_mod)
     print('[Without Conversion Suffix]')
     print(func)
     mod['main'] = func
@@ -120,6 +199,7 @@ def quantize(mod, params):
       nbit_activation=8,
       dtype_activation="int8",
       skip_conv_layers=[],
+      skip_dense_layers=False,
       dtype_input="int8",
       dtype_weight="int8"):
         print('input:', mod)
@@ -156,6 +236,12 @@ def quantize_cifar10():
   quantize(mod, params)
 
 
+def get_param_type(func, name):
+    for param in func.params:
+        if param.name_hint == name:
+            return param.checked_type
+
+
 def test_conv_quant():
     dshape = (1, 4, 16, 16)
     # TODO quantization shouldn't crash if a pre-quantized graph (already int8) is handed to it.
@@ -169,10 +255,8 @@ def test_conv_quant():
             channels=4)
     func = relay.Function(relay.analysis.free_vars(conv_expr), conv_expr)
     mod = tvm.IRModule.from_expr(func)
-    mod = transform.InferType()(mod)
 
     # TODO crash is probs coming from non-const weights? add param for weight
-    import numpy as np
     weight_ty = mod['main'].params[1].type_annotation
     w_shape = list(map(lambda x: x.value, mod['main'].params[1].checked_type.shape))
     params = {
@@ -190,7 +274,6 @@ def test_add_quant():
     }
     """)
     mod = tvm.IRModule.from_expr(func)
-    mod = transform.InferType()(mod)
     params = {}
     quantize(mod, params)
     # """
@@ -203,7 +286,40 @@ def test_add_quant():
     # f = relay.Function([x], sb.get())
 
 
+def test_multiple_arg_conversions():
+    dshape = (1, 4, 16, 16)
+    # TODO quantization shouldn't crash if a pre-quantized graph (already int8) is handed to it.
+    dtype = 'float32'
+    func_name = 'main'
+    conv1 = relay.nn.conv2d(
+        relay.var("x1", shape=dshape, dtype=dtype),
+        relay.var("w1"),
+        kernel_size=(3, 3),
+        padding=(1, 1),
+        channels=4)
+    conv2 = relay.nn.conv2d(
+        relay.var("x2", shape=dshape, dtype=dtype),
+        relay.var("w2"),
+        kernel_size=(3, 3),
+        padding=(1, 1),
+        channels=4)
+    res = relay.add(conv1, conv2)
+    func = relay.Function(relay.analysis.free_vars(res), res)
+    mod = tvm.IRModule.from_expr(func)
+
+    w1_ty = get_param_type(mod['main'], 'w1')
+    w2_ty = get_param_type(mod['main'], 'w2')
+    w1_shape = list(map(lambda x: x.value, w1_ty.shape))
+    w2_shape = list(map(lambda x: x.value, w2_ty.shape))
+    params = {
+        'w1': tvm.nd.array(np.random.uniform(0, 1, size=w1_shape).astype(w1_ty.dtype), ctx=tvm.cpu(0)),
+        'w2': tvm.nd.array(np.random.uniform(0, 1, size=w2_shape).astype(w2_ty.dtype), ctx=tvm.cpu(0))
+    }
+    quantize(mod, params)
+
+
 if __name__ == '__main__':
-  quantize_cifar10()
-#   test_conv_quant()
-#   test_add_quant()
+    # quantize_cifar10()
+    # test_conv_quant()
+    # test_add_quant()
+    test_multiple_arg_conversions()
